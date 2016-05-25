89a80c1a589e7d62b6e4a0ed9ca0290c3b90079c
TusharSachde <tushar@wohlig.com>
2016-05-25 12:58:40 +0530

    Initial Commit

---
+++ DemoWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DemoWatch WatchKit Extension
+//
+//  Created by Tushar Sachde on 25/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
